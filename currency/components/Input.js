804e9d5a190f44cbd1d140ea662b9386a9537fe0
Denis <denisb.oliveira@live.com>
2019-12-05 22:06:06 -0300

    Requisição API corrigida

--- currency/components/Input.js
+++ currency/components/Input.js
@@ -1,30 +1,40 @@
 // https://exchangeratesapi.io/
 
-import React from 'react'
+import React, {Component} from 'react'
 import {Text, View} from 'react-native'
 
-const request = config => {    
-    return fetch(config.url, config.method)
-    .then(response => response.json())
-    .then(response => config.success(response))
-}
-
-const Input = props => {
-    const config = {
+class Input extends Component {
+    config = {
         method: "GET",
-        url: `https://api.exchangeratesapi.io/latest?base=${props.from}`,
-        success: response => response.rates[props.to]
+        url: `https://api.exchangeratesapi.io/latest?base=${this.props.from}`,
+        success: response => response.rates[this.props.to],
+        error: error => console.error(error)
     }
 
-    request(config).then(value => {
-        // 1. o valor fica preso na promisse
-        {console.log(value)}
-    })
+    state = {
+        data: ""
+    }
 
-    return (
-        // 2. ele deveria ser colocado no return do componente
-        <View><Text>{`Resposta: `}</Text></View>
-    )
+    componentDidMount = () => {
+        fetch(this.config.url, {method: this.config.method})
+        .then((response) => response.json())
+        .then((responseJson) => {
+            this.setState({
+                data: this.config.success(responseJson)
+            })
+        })
+        .catch((error) => {
+            this.config.error(error)
+        })
+    }
+
+    render() {
+        return (
+           <View>
+              <Text>{`Resposta:  ${this.state.data}`}</Text>
+           </View>
+        )
+     }
 }
 
-export default Input;
+export default Input
