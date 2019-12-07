66c4f0f277eb447f32e9f700eb711687c93bf982
leojacondev <geral@leojacon.com>
2019-12-07 12:20:28 -0300

    Tentando adicionar o picker

--- currency/App.js
+++ currency/App.js
@@ -1,13 +1,54 @@
-import React from 'react'
-import {Text, View} from 'react-native'
+import React, {useState} from 'react'
+import {Text, View, Picker} from 'react-native'
 
 import Input from './components/Input'
 
 const App = () => {
+
+  const [toParamether, setTo] = useState("");
+  const [fromParamether, setFrom] = useState("");
+
+  const updateTo = (to) =>{
+      setTo(to);
+  }
+
+    const updateFrom = (from) =>{
+      setFrom(from)
+      renderResponse(fromParamether, toParamether);
+  }
+
+  const renderResponse = (to, from) =>{
+    return(
+    <View>
+      <Input from={from} to={to}></Input>
+    </View>
+    )
+  }
+
   return (
     <View>
       <Text>Página inicial</Text>
-      <Input from={"USD"} to={"BRL"}></Input>
+
+  <Picker
+  selectedValue={fromParamether}
+  style={{height: 50, width: 100}}
+  onValueChange={(itemValue, itemIndex) =>
+    updateFrom(itemValue)
+  }>
+  <Picker.Item label="Dolar Americano" value="USD" />
+  <Picker.Item label="Real" value="BRL" />
+</Picker>
+
+<Picker
+  selectedValue={toParamether}
+  style={{height: 50, width: 100}}
+  onValueChange={(itemValue, itemIndex) =>
+    updateTo(itemValue)
+  }>
+  <Picker.Item label="Dolar Americano" value="USD" />
+  <Picker.Item label="Real" value="BRL" />
+</Picker>
+    {renderResponse(toParamether, fromParamether)}
     </View>
   )
 }
